949c1f07ac129c714486ae91731eb39722b0af41
Andrew Jensen <jensen.aj@outlook.com>
2021-03-22 21:07:58 -0500

    inefficient solution to 0037

--- 0037_TruncatablePrimes.py
+++ 0037_TruncatablePrimes.py
@@ -1,70 +1,35 @@
 import primetools as pt
 
-digits = str(list(range(1,10)))
-print(digits)
-prime_digits = [2, 3, 5, 7]
 
-primes = pt.getPrimes(1000000)
-
-def truncate(from_left=True):
+def truncate(p: int, from_left=True):
+    ''' Repeatedly truncate the number. Return false if any truncated number is not prime. '''
+    digits = [c for c in str(p)]
     while len(digits) != 0:
         if int(''.join(digits)) not in primes:
             return False
         else:
-            if from_left=True:
+            if from_left==True:
                 digits = digits[:-1]
             else:
                 digits = digits[1:]
-
-def is_truncatable(p):
-    digits = [c for c in str(p)]
-    even = True
-    digits2 = digits.copy()
-    while len(digits) != 0:
-        digits_as_num = int(''.join(digits))
-        if digits_as_num not in primes:
-            return False
-        if even:
-            digits = digits[:-1]
-        else:
-            digits = digits[1:]
-        if len(digits) == 1:
-            if int(digits[0]) not in prime_digits:
-                return False
-    even = False
-    while len(digits2) != 0:
-        digits_as_num = int(''.join(digits2))
-        if digits_as_num not in primes:
-            return False
-        if even:
-            digits2 = digits2[:-1]
-        else:
-            digits2 = digits2[1:]
-        if len(digits2) == 1:
-            if int(digits2[0]) not in prime_digits:
-                return False
     return True
 
-# for p in primes:
-#     if is_truncatable(p) and p not in prime_digits:
-#         print(p)
+def is_truncatable(p: int):
+    ''' Returns whether the prime `p` is truncatable from the left and the right. '''
+    return truncate(p, True) and truncate(p, False)
 
-# The solution.
-print(sum(
-    [
-     23 ,  
-37    ,
-53    ,
-73    ,
-313   ,
-317   ,
-373   ,
-797   ,
-3137  ,
-3797  ,
-739397,
-    ]
-))
+def get_first_11_truncatable_primes(primes):
+    ''' First, generate a bunch of prime numbers. Go through each one in order and determine if it is truncatable until we hit 11. '''
+    primes = pt.getPrimes(1000000)
+    truncatable_primes = []
+    num_truncatable_primes_found = 0
+    for p in primes:
+        if num_truncatable_primes_found == 11:
+            return truncatable_primes
+        if is_truncatable(p) and p not in [2, 3, 5, 7]:
+            num_truncatable_primes_found += 1 
+            truncatable_primes.append(p)
+            print(p)
+    return []
 
-print(is_truncatable(3797))
-print(is_truncatable(739397))
+print(get_first_11_truncatable_primes())
